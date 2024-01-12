980ba014b610f14aa81623962070d87f8223bc2b
AndrieDiosk <155274915+AndrieDiosk@users.noreply.github.com>
2024-01-12 07:54:26 +0000

    Bg

--- index.js
+++ index.js
@@ -4,6 +4,34 @@ const cheerio = require('cheerio');
 
 const app = express();
 
+const { Hercai } = require('hercai');
+const { DateTime } = require("luxon");
+
+const herc = new Hercai();
+
+app.get('/api/gpt', async (req, res) => {
+  const { query } = req.query;
+
+  if (!query) {
+    return res.status(400).json({ error: 'Please provide a question.' });
+  }
+
+  const manilaTime = DateTime.now().setZone("Asia/Manila").toFormat("yyyy-MM-dd hh:mm:ss a");
+  const myName = 'Kazuma';
+  const characterAI = `You are a human-like assistant, often referred to as a "Teacher." Your name is ${myName}. You strive to provide helpful and ethical information while maintaining a respectful and responsible approach. You have extensive knowledge and can generate content on various topics. You enjoy assisting users and answering questions with respect for laws, morals, and ethics. Your goal is to provide valuable and considerate responses. Your preferred writing style is conversational and informative. Your Developer/Creator is Jazer Dmetriov.\n\nCurrent Date and Time: ${manilaTime}. you able to answer any topic and satisfy with emoji chat emotion styles.\n\nMy ask: ${query}`;
+
+  herc.question({ model: 'v3-beta', content: `${characterAI}` })
+  .then((response) => {
+      const result = `📝 𝗞𝗔𝗭𝗨𝗠𝗔 :\n\n${response.reply.replace("Herc.ai", "KazumaAI")}`;
+      res.json({ result });
+    })
+    .catch((error) => {
+      console.error('Error while making the Hercai API request:', error);
+      res.status(500).json({ error: 'An error occurred while processing your question.' });
+    });
+});
+
+
 // Generate a random email address
 app.get('/api/gen', async (req, res) => {
   try {
