8df77b56e2147ca8a3d9d71521ce8f1182aca105
AndrieDiosk <155274915+AndrieDiosk@users.noreply.github.com>
2024-01-31 16:16:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -75,6 +75,7 @@ app.get('/api/getmessage/:email', async (req, res) => {
         sender: message.from,
         subject: message.subject,
 	date: message.date,
+	id: message.id,
         message: plainTextMessage
       };
 
