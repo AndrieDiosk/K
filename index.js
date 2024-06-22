4b037b653fb24f02d81ccc7a752dd53ee01a7ea8
AndrieDiosk <155274915+AndrieDiosk@users.noreply.github.com>
2024-06-22 04:24:19 +0000

    H

--- index.js
+++ index.js
@@ -246,124 +246,6 @@ app.get("/v2/tiktok", async (req, res) => {
  }
 });
 
-let sessionId, cookies;
-const mainCookie = 'fghflfT87fogT4T5903K8dM8dZ5KxCfuExtCH_Nqbn8KsYaqWTx_UFVFuV8sTvElW4HFvQ.'; // Replace with your own cookie
-
-class BardAI {
-  constructor() {
-    this.cookie = mainCookie;
-    if (!this.cookie) throw new Error("Session Cookies are missing, Unable to login to an account!");
-  }
-
-  async login() {
-    cookies = this.cookie;
-    let headerParams = {
-      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-      "Cookie": `__Secure-1PSID=${this.cookie};`
-    };
-    let instance = axios.create({
-      withCredentials: true,
-      baseURL: "https://bard.google.com/",
-      headers: headerParams
-    });
-
-    try {
-      let r = await instance.get();
-      sessionId = r.data.match(/SNlM0e":"(.*?)"/g)[0].substr(8).replace(/\"/g, '');
-    } catch (e) {
-      throw new Error('Unable to login to your account. Please try using new cookies and try again.');
-    }
-  }
-}
-
-let imageFormat = (text, images) => {
-  if (!images) return { message: text, imageUrls: [] };
-  let formattedText = text.replace(/\[Image of.*?\]/g, '').trim();
-  images.forEach(imageData => {
-    imageData.tag = imageData.tag.replace(/\[Image of.*?\]/g, "").trim();
-  });
-  return { message: formattedText, imageUrls: images.map((image) => image.url) };
-};
-
-let startBard = async (message) => {
-  if (!sessionId) throw new Error('Please initialize login first to use bardai.');
-  let postParamsStructure = [
-    [message],
-    null,
-    [],
-  ];
-  let postData = {
-    "f.req": JSON.stringify([null, JSON.stringify(postParamsStructure)]),
-    at: sessionId
-  };
-  let headerParams = {
-    "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-    "Cookie": `__Secure-1PSID=${cookies};`
-  };
-
-  try {
-    let r = await axios({
-      method: 'POST',
-      url: 'https://bard.google.com/_/BardChatUi/data/assistant.lamda.BardFrontendService/StreamGenerate?bl=boq_assistant-bard-web-server_20230711.08_p0&_reqID=0&rt=c',
-      headers: headerParams,
-      withCredentials: true,
-      data: postData
-    });
-    let bardAIRes = JSON.parse(r.data.split("\n")[3])[0][2];
-    if (!bardAIRes) throw new Error(`Bard AI encountered an error ${r.data}.`);
-    let bardData = JSON.parse(bardAIRes);
-    let bardAI = JSON.parse(bardAIRes)[4][0];
-    let result = bardAI[1][0];
-    let images = bardAI[4]?.map(e => {
-      return {
-        url: e[3][0][0],
-        tag: e[2],
-        source: {
-          name: e[1][1],
-          original: e[0][0][0],
-          website: e[1][0][0],
-          favicon: e[1][3]
-        }
-      };
-    });
-    return imageFormat(result, images);
-  } catch (error) {
-    throw new Error(`Bard AI encountered an error ${error.message}.`);
-  }
-};
-
-app.get('/api/tools/bard', async (req, res) => {
-  const { question } = req.query;
-  try {
-    const bard = new BardAI();
-    await bard.login();
-    
-    let answer = '';
-
-    // Custom responses for specific queries
-    if (question.includes('who are you?')) {
-      answer = "I'm a ChatBot Created By Jazer Dmetriov. Nice to meet you!";
-    } else if (question.includes('who created you?')) {
-      answer = "I was created by Jazer Dmetriov.";
-    } else if (question.includes('who created you')) {
-      answer = "I was created by Jazer Dmetriov.";
-    } else if (question.includes('who are you')) {
-      answer = "I'm a ChatBot Created By Jazer Dmetriov. Nice to meet you!";
-    }
-
-    if (answer !== '') {
-      res.json({ message: answer, imageUrls: [] });
-      return;
-    }
-
-    const response = await startBard(question);
-    const { message, imageUrls } = response;
-    res.json({ message, imageUrls });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
 // Serve the documentation page
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -381,7 +263,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
